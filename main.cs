97008393dbb59a5b5c332859124622af39dfa5d9
farvillage <ygorsoares2003@gmail.com>
2023-09-12 14:55:47 -0300

    eu amo c#

--- main.cs
+++ main.cs
@@ -2,21 +2,20 @@ using System;
 
 public class Person
 {
-    public string name;
-    public int age;
-
-    public Person(String name, int age)
+    protected string name { get; private set; }
+    protected int age { get; private set; }
+    public Person(string name, int age)
     {
         this.name = name;
         this.age = age;
     }
+
 }
 public class Teacher : Person
 {
-    public string graduation;
-    public int salary;
-
-    public Teacher(string graduation, int salary)
+    protected string graduation { get; private set; }
+    protected int salary { get; private set; }
+    public Teacher(string name, int age, string graduation, int salary) : base(name, age)
     {
         this.graduation = graduation;
         this.salary = salary;
@@ -24,59 +23,64 @@ public class Teacher : Person
 }
 public class Student : Person
 {
-    public double grade01;
-    public double grade02;
-    public double averageGrade;
-    public Student(double grade01, double grade02)
+    protected double grade01 { get; private set; }
+    protected double grade02 { get; private set; }
+
+    public Student(string name, int age, double grade01, double grade02) : base(name, age)
     {
         this.grade01 = grade01;
         this.grade02 = grade02;
-        this.averageGrade = (grade01 + grade02) / 2;
+    }
+    public double averageGrade(double grade01, double grade02)
+    {
+        return (grade01 + grade02) / 2;
     }
 }
 public partial class Program
 {
     public static void Main(string[] args)
     {  
-        Person p = new Person();
-        Teacher t = new Teacher();
-        Student s = new Student();
-
         System.Console.WriteLine("Input 1 for Teacher or 2 for Student: ");
-        System.Console.ReadLine();
+        int register = Convert.ToInt32(System.Console.ReadLine());
 
         switch (register)
         {
             case 1:
             System.Console.WriteLine("Enter your name: ");
-            p.name = System.Console.ReadLine();
+            string name = System.Console.ReadLine();
 
             System.Console.WriteLine("Enter your age: ");
-            int p.age = Convert.ToInt32(System.Console.ReadLine());
+            int age = Convert.ToInt32(System.Console.ReadLine());
 
             System.Console.WriteLine("Enter your graduation: ");
-            t.graduation = System.Console.ReadLine();
+            string graduation = System.Console.ReadLine();
 
             System.Console.WriteLine("Enter your salary: ");
-            int t.salary = Convert.ToInt32(System.Console.ReadLine());
+            int salary = Convert.ToInt32(System.Console.ReadLine());
+
+            Teacher t = new Teacher(name, age, graduation, salary);
             break;
 
             case 2:
             System.Console.WriteLine("Enter your name: ");
-            s.name = System.Console.ReadLine();
+            name = System.Console.ReadLine();
 
             System.Console.WriteLine("Enter your age: ");
-            int s.age = Convert.ToInt32(System.Console.ReadLine());
+            age = Convert.ToInt32(System.Console.ReadLine());
 
             System.Console.WriteLine("Enter your Grade 01: ");
-            int s.grade01 = Convert.ToInt32(System.Console.ReadLine());
+            double grade01 = Convert.ToInt32(System.Console.ReadLine());
 
             System.Console.WriteLine("Enter your Grade 02: ");
-            int s.grade02 = Convert.ToInt32(System.Console.ReadLine());
+            double grade02 = Convert.ToInt32(System.Console.ReadLine());
+
+            Student s = new Student(name, age, grade01, grade02);
+            System.Console.WriteLine("Your average grade is: " + s.averageGrade(grade01, grade02));
             break;
 
             default:
-            Console.ReadLine("none");
+            Console.WriteLine("none");
+            break;
         }
     }
 }
